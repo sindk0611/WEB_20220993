4d8d1ff5c045eb4e368dba424dbe4b7b3c3eedd6
신동규 <sindk0611@sungkyul.ac.kr>
2023-04-05 05:32:36 +0000

    5주차

--- js/search.js
+++ js/search.js
@@ -1,5 +1,9 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
 function search_message(){
-   alert("검색을 수행합니다!"); 
+   alert("검색을 수행합니다!");    
+   let search_str = document.querySelector("#search_txt"); // 변수에 저장
+   document.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가
+   console.log(search_str.value); // 콘솔에 출력
+
 }
